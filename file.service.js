d3338f6c067d0b03025c76380e945ff7cb5bbc74
[name] <jaipal@createbytes.com>
2023-12-13 02:06:11 +0530

    added validation in inputs of filepath and questions

--- file.service.js
+++ file.service.js
@@ -2,7 +2,6 @@ import fs from "fs/promises";
 export const readFileContent = async (fileName) => {
   try {
     const fileData = await fs.readFile(`${fileName}`, { encoding: "utf-8" });
-    console.log('fileData: ', fileData);
     return fileData;
   } catch (error) {
     console.log({ err: error });
--- utils/constants.js
+++ utils/constants.js
@@ -2,11 +2,17 @@ export const FILE_READING_CONTENT = [
     {
       type: "input",
       name: "fileName",
-      message: "Give Path to the file which you wanted to read!",
+      message: "Give path root to the file needs to be explain !",
+      validate: (input) => {
+        return input.trim() !== "" ? true : "File path cannot be empty !";
+      },
     },
     {
       type: "input",
       name: "question",
-      message: "Ask a question that is relevant!",
+      message: "Ask any revelent question about it !",
+      validate: (input) => {
+        return input.trim() !== "" ? true : "Question cannot be empty !";
+      },
     },
   ]
